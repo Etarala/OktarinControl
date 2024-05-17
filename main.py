eb161df3bba0c3e3ae882629f1459a01f60fa361
Etarala <etarala@mail.ru>
2024-05-17 14:07:12 +0300

    Ver 1.2 Переименование строк, условие количества повторений

--- main.py
+++ main.py
@@ -6,28 +6,43 @@ df = pd.read_excel('1.xlsx')
 # Преобразование значений в числовой формат для столбцов 3 и 13
 df.iloc[:, [2, 12]] = df.iloc[:, [2, 12]].apply(pd.to_numeric, errors='coerce')
 
+# Переименование столбцов
+df.rename(columns={
+    'Unnamed: 2': '*Счет*',
+    'Unnamed: 3': '*Владелец*',
+    'Unnamed: 4': '*Карта*',
+    'Клиент: Частные лица': '*АЗС*',
+    'Unnamed: 7': '*Вид топлива*',
+    'Unnamed: 10': '*Сумма покупки*',
+    'Unnamed: 12': '*Начислено бонусов*',
+    'Unnamed: 15': '*Тип операции*'
+}, inplace=True)
+
 # Фильтрация строк по условиям
-filtered_df = df[df.iloc[:, 15] == 'Начисление']
+filtered_df = df[df['*Тип операции*'] == 'Начисление']
 
-# Удаление столбцов с именами
+# Удаление ненужных столбцов
 columns_to_drop = [
-    df.columns[1],  # Столбец 1
-    df.columns[6],  # Столбец 6
-    df.columns[8],  # Столбец 8
-    df.columns[9],  # Столбец 9
-    df.columns[11], # Столбец 11
-    df.columns[13], # Столбец 13
-    df.columns[14], # Столбец 14
-    df.columns[16]  # Столбец 16
+    'Unnamed: 1',  # Столбец 1
+    'Unnamed: 6',  # Столбец 6
+    'Unnamed: 8',  # Столбец 8
+    'Unnamed: 9',  # Столбец 9
+    'Unnamed: 11', # Столбец 11
+    'Unnamed: 13', # Столбец 13
+    'Unnamed: 14', # Столбец 14
+    'Unnamed: 16'  # Столбец 16
 ]
-filtered_df = filtered_df.drop(columns=columns_to_drop)
+filtered_df.drop(columns=columns_to_drop, inplace=True)
 
-# Группировка по третьему столбцу (его имя) и итерация по группам
-group_column_name = df.columns[2]
-sum_column_name = df.columns[12]
+# Подсчет количества повторений для каждого значения в столбце "Счет"
+counts = filtered_df['*Счет*'].value_counts()
 
-for _, group_df in filtered_df.groupby(filtered_df[group_column_name]):
+# Фильтрация по количеству повторений
+filtered_df = filtered_df[filtered_df['*Счет*'].isin(counts.index[counts > 1])]
+
+# Группировка по столбцу "Счет" и итерация по группам
+for _, group_df in filtered_df.groupby('*Счет*'):
     # Вывод строк с текущим повторяющимся значением
     print(group_df.to_string(index=False))
-    # Вывод суммы значений в столбце с нужным именем для текущей группы
-    print(f"Сумма значений в столбце 13: {group_df[sum_column_name].sum()}\n")
+    # Вывод суммы значений в столбце "Количество бонусов" для текущей группы
+    print(f"Всего начислено бонусов по счету: {group_df['*Начислено бонусов*'].sum()}\n")
